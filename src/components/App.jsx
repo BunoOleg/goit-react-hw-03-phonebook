3b3f39ad954f53b00866a18fff92a5e9b42eda6f
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-07-13 17:40:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,6 +58,21 @@ class App extends Component {
     );
   };
 
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const filteredContacts = this.getFilteredContacts();
 
